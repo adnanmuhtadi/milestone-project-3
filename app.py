cf9bd04e26340e8989eeb4eee5f8f8c18e8ca711
adnanmuhtadi <adnanmuhtadi96@gmail.com>
2021-03-29 13:33:35 +0000

    Mod - Added database information as well as a function to test the connection

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
 
+# To get hold of the database
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# Setting up an instance of PyMongo and adding the app into the constructor method
+mongo = PyMongo(app)
+
+@app.route("/")
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 # This tells the application where and how to run.
 if __name__ == "__main__":
