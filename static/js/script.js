8e39fea3806275081f89261e9e27e0f697f24165
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-04-06 23:12:36 +0100

    Mod - Added the carousel and amended Navigations
    Added the carousel for the home page as well as amended the colours for the navigations

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     //for the Navbar
-    $(".sidenav").sidenav({edge: "right"});   
-}); 
+    $(".sidenav").sidenav({ edge: "right" });
+    //for the carousel
+    $(".carousel").carousel();
+
+    //Carousel function which would go round on a set timer
+    setInterval(function () {
+        $('.carousel').carousel('next');
+
+    }, 2000);
+});
