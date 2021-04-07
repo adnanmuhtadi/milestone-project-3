bdb6767a87f1760c177e7136d0adcd7480510550
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-04-07 09:35:45 +0100

    Mod - Adding the function for registration
    Linking and adding the function for the registration with db validation to ensure the username is newly created.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -17,11 +18,32 @@ app.secret_key = os.environ.get("SECRET_KEY")
 # Setting up an instance of PyMongo and adding the app into the constructor method
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def registration():
+    if request.method == "POST":
+        # checking if username already exists in the db
+        user_exisiting = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        # producing error message if user exists
+        if user_exisiting:
+            flash("Username already exists")
+            return redirect(url_for("registration"))
+
+        # data from the form will post acting as the post method
+        registration = {
+            "username": request.form.get("username")
+        }
+    return render_template("registration.html")
+
 
 # This tells the application where and how to run.
 if __name__ == "__main__":
